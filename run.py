a0ecf0197337d60ede39037f4999e6537e428194
Aspen92 <asplundchristofer@gmail.com>
2022-10-27 19:05:05 +0000

    Make the full list of movies to apear in the right raiting order

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 import os
+from pprint import pprint
 clear = lambda: os.system('clear')
 
 
@@ -15,14 +16,17 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('movie_ratings_p3')
 
-titel_ratings = SHEET.worksheet('titel_ratings')
+title_ratings = SHEET.worksheet('title_ratings')
+
+
 
 
 menu_options = {
     1: 'Show Full Movies List',
     2: 'Show Top 10 Movies',
     3: 'Add New Movie & Rating',
-    4: 'Exit Program',
+    4: 'Delete a Movie From the List',
+    5: 'Exit Program',
 }
 
 
@@ -31,40 +35,52 @@ menu_options = {
 """
 
 def print_menu():
+    """
+    """
     print("Welcome movie blabla")
     print("Enter options 1-4")
     for key in menu_options.keys():
         print(key, '--', menu_options[key])
 
 def get_full_list():
+    """
+    """
     clear()
-    movies = titel_ratings.get_all_records()
-    for movie in movies:
-        print(movie)
+    movies = title_ratings.get_all_records()
+    full_list = sorted(movies, key=lambda d: d['Ratings'], reverse=True)
+    for movie in full_list:
+        print(f'Title: {movie["Title"]} - Ratings: {movie["Ratings"]}')
     input('Press Enter to Return to Menu: ')
     clear()
 
 def get_top_ten():
+    """
+    """
     clear()
-    movies = titel_ratings.get_all_records()
+    print("Here is the Top 10 Movies")
+    movies = title_ratings.get_all_records()
     newlist = sorted(movies, key=lambda d: d['Ratings'], reverse=True)
-    for movie in newlist:
-        print(movie)
+    for movie in newlist[0:10]:
+        print(f'Title: {movie["Title"]} - Ratings: {movie["Ratings"]}')
+    input('Press Enter to Return to Menu: ')
+    clear()
 
 def update_worksheet():
     """
-    Receives a list of integers to be inserted into a worksheet
-    Update the relevant worksheet with the data provided
     """
     clear()
     title = input('Enter Movie Title: ')
-    raiting = input('Enter Movie Raiting: ')
+    raiting = input('Enter Movie Rating (Use only point numbers ex. 4.3): ')
     worksheet_to_update = SHEET.worksheet("titel_ratings")
     worksheet_to_update.append_row([title, raiting])
-    print(f"Added - Movie: {title} Raiting: {raiting}")
+    print(f"Added - Movie: {title} Rating: {raiting}")
+    input('Press Enter to Return to Menu: ')
+    clear()
 
 
 def main():
+    """
+    """
     while(True):
         print_menu()
         option = int(input('Enter your choice: ')) 
@@ -75,6 +91,8 @@ def main():
         elif option == 3:
             update_worksheet()
         elif option == 4:
+            print('Delete')
+        elif option == 5:
             print('Thanks message before exiting')
             exit()
         else:
