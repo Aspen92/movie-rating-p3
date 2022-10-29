76093bf65f0639fa9b6dad4ff5cfb4cb11a6f0fb
Aspen92 <asplundchristofer@gmail.com>
2022-10-29 10:49:28 +0000

    Added comments to the different functions and sections

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 """
-asd
+Imports.
 """
 import os
 import gspread
@@ -9,27 +9,27 @@ from google.oauth2.service_account import Credentials
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/drive",
 ]
 
-CREDS = Credentials.from_service_account_file('creds.json')
+CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('movie_ratings_p3')
+SHEET = GSPREAD_CLIENT.open("movie_ratings_p3")
 
-title_ratings = SHEET.worksheet('title_ratings')
+title_ratings = SHEET.worksheet("title_ratings")
 menu_options = {
-    1: 'Show Full Movies List',
-    2: 'Show Top 10 Movies',
-    3: 'Add New Movie & Rating',
-    4: 'Delete a Movie From the List',
-    5: 'Exit Program',
+    1: "Show Full Movies List",
+    2: "Show Top 10 Movies",
+    3: "Add New Movie & Rating",
+    4: "Delete a Movie From the List",
+    5: "Exit Program",
 }
 
 
 def print_menu():
     """
-    asda
+    Prints welcome message, info message and options menu to user from a for loop.
     """
     print("\n    WELCOME TO YOUR OWN MOVIE DATABASE PROGRAM!")
     print("    -------------------------------------------")
@@ -43,74 +43,78 @@ def print_menu():
     print(welcome_msg)
     print("Enter options 1-4\n")
     for key, value in menu_options.items():
-        print(key, '--', value)
+        print(key, "--", value)
 
 
 def clear():
     """
-    asd
+    Function that clears the output.
     """
-    os.system('clear')
+    os.system("clear")
 
 
 def get_full_list():
     """
-    asda
+    Function with a for loop that gets all movies from google sheets sorted in ratings order from best to worst
+    and prints the list to the output.
     """
     clear()
     print("ALL MOVIES:\n")
     movies = title_ratings.get_all_records()
-    full_list = sorted(movies, key=lambda d: d['Ratings'], reverse=True)
+    full_list = sorted(movies, key=lambda d: d["Ratings"], reverse=True)
     for movie in full_list:
         print(f'Title: {movie["Title"]}\nRatings: {movie["Ratings"]}\n')
-    input('\nPress Enter to Return to Menu:\n')
+    input("\nPress Enter to Return to Menu:\n")
     clear()
 
 
 def get_top_ten():
     """
-    asd
+    Function with a for loop that gets the top 10 movies with the best rating(best-worst)
+    and prints the list to the output.
     """
     clear()
     print("TOP 10 MOVIES\n")
     movies = title_ratings.get_all_records()
-    newlist = sorted(movies, key=lambda d: d['Ratings'], reverse=True)
+    newlist = sorted(movies, key=lambda d: d["Ratings"], reverse=True)
     for movie in newlist[0:10]:
         print(f'Title: {movie["Title"]}\nRatings: {movie["Ratings"]}\n')
-    input('Press Enter to Return to Menu:\n')
+    input("Press Enter to Return to Menu:\n")
     clear()
 
 
 def update_worksheet():
     """
-    asd
+    Function with if/else statements for the 'Add New Movie & Rating option'.
+    It let's you enter a movie title that is a string and a raiting that is a float number.
+    Then adds the new movie to the google sheet and gives it a unique ID number.
     """
     clear()
-    title = input('Enter Movie Title: ')
-    rating = input('Enter Movie Rating (0.0 - 5.0): ')
+    title = input("Enter Movie Title: ")
+    rating = input("Enter Movie Rating (0.0 - 5.0): ")
     if validate_input_rating(rating):
         if float(rating) <= 5.0:
             worksheet_to_update = SHEET.worksheet("title_ratings")
             movies = title_ratings.get_all_records()
-            max_value = max(movies, key=lambda x: int(x['Movie_ID']))
-            new_id = int(max_value['Movie_ID']) + 1
+            max_value = max(movies, key=lambda x: int(x["Movie_ID"]))
+            new_id = int(max_value["Movie_ID"]) + 1
             worksheet_to_update.append_row([title, rating, new_id])
             print(f"\nADDED\nMovie: {title}\nRating: {rating}")
-            input('\nPress Enter to Return to Menu: ')
+            input("\nPress Enter to Return to Menu: ")
             clear()
         else:
             clear()
             print("Invalid data: Rating must be a float between 0.0 - 5.0.\n")
-            input('\nPress Enter to return to add new movie option: ')
+            input("\nPress Enter to return to add new movie option: ")
             update_worksheet()
     else:
-        input('\nPress Enter to return to add new movie option: ')
+        input("\nPress Enter to return to add new movie option: ")
         update_worksheet()
 
 
 def validate_input_rating(value):
     """
-    asd
+
     """
     try:
         float(value)
@@ -124,16 +128,17 @@ def validate_input_rating(value):
 
 def delete_movie():
     """
-    asd
+    Function with a for loop that prints a list of all movies with the ID numbers and 
+    let's you delete a movie from the list by entering the movie ID number.
     """
     clear()
     print("ALL MOVIES:\n")
     movies = title_ratings.get_all_records()
-    full_list = sorted(movies, key=lambda d: d['Ratings'], reverse=True)
+    full_list = sorted(movies, key=lambda d: d["Ratings"], reverse=True)
     for movie in full_list:
         print(f'Title: {movie["Title"]}\nRatings: {movie["Ratings"]}')
         print(f'Movie ID: {movie["Movie_ID"]}\n')
-    movie_id = input('Enter Movie ID: ')
+    movie_id = input("Enter Movie ID: ")
     row = title_ratings.find(movie_id, in_column=3).row
     title_ratings.delete_rows(row)
     clear()
@@ -141,12 +146,12 @@ def delete_movie():
 
 def main():
     """
-    asd
+    Main function that runs the different options functions.
     """
     while True:
         print_menu()
         try:
-            option = int(input('\nEnter your choice:\n'))
+            option = int(input("\nEnter your choice:\n"))
             if option == 1:
                 get_full_list()
             elif option == 2:
@@ -156,14 +161,15 @@ def main():
             elif option == 4:
                 delete_movie()
             elif option == 5:
-                print('Thanks message before exiting')
+                print("Thanks message before exiting")
                 exit()
             else:
-                print('Invalid option. Please enter a number between 1 and 4.')
+                print("Invalid option. Please enter a number between 1 and 4.")
         except ValueError:
             clear()
             print("Invalid option. Please enter a number between 1 and 4.\n")
-            input('\nPress Enter to return to menu: ')
+            input("\nPress Enter to return to menu: ")
 
 
-main()
+if __name__ == "__main__":
+    main()
